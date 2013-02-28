8ff720f6e8757d71e0d5b07d14f56895f9380e23
ludoblues <ludovic.vue@gmail.com>
2013-02-28 08:01:19 +0100

    first

---
+++ source/Client/js/app.js
@@ -0,0 +1,138 @@
+var App = {
+    Scenarios: {
+	victory: function(data) {
+	    App.Clock.stop();
+	    $('h1#status-game').text('Player '+data.user_id+' won!');
+	},
+	
+	equality: function(data) {
+	    App.Clock.stop();
+	    $('h1#status-game').text('Anybody won!');
+	}
+    },
+
+    Clock: {
+	countdown: 0,
+	delay: 10,
+	
+	init: function(user_id, delay) {
+	    this.delay = delay;
+	    $('h1#status-game').text(App.Player.id == user_id ? 'It\'s yout turn to play!' : 'Your challenger is thinking...');
+	    this.start(App.Player.id == user_id);
+	},
+
+	start: function(is_my_turn) {
+	    console.log('|-->clock starts delay['+this.delay+']');
+	    var self = this;
+	    this.countdown = setInterval(function() {
+		if (!self.delay) {
+		    alert('Too late !');		    
+		}
+		self.delay--;
+		$('time#clock').text(self.delay);
+	    }, 1000);
+
+	    console.log('|-->countdown['+this.countdown+'] launched');
+	    if (is_my_turn) {
+		App.Playground.unlock();
+		return true;
+	    }
+	    App.Playground.lock();
+	},
+	
+	stop: function() {
+	    clearInterval(this.countdown);
+	    App.Playground.lock();
+	},
+
+	reset: function(is_my_turn) {
+	    this.delay = 10;
+	    if (is_my_turn) {
+		App.Playground.unlock();
+		return true;
+	    }
+	    App.Playground.lock();
+	}
+    },
+
+    Playground: {
+	lock: function() {
+	    $('td.square').unbind('click', this.play);
+	},
+	
+	unlock: function() {
+	    $('td.square.free').bind('click', this.play);
+	},
+	
+	//TODO: sortir ce socket callback d'ici
+	play: function() {
+	    this.className = this.className.replace('free', 'played');
+	    this.id = App.Player.id;
+	    console.log('pos['+this.getAttribute('data-x')+', '+this.getAttribute('data-y')+'] played by player['+App.Player.id+']');
+	    App.Player.play({ x: this.getAttribute('data-x'), y: this.getAttribute('data-y') });
+	},
+
+	update: function(data) {
+	    if (data.user_id == App.Player.id) {
+		App.Clock.reset(false);
+		console.log('case updated by its own');
+		$('h1#status-game').text('Your challenger is thinking...');
+		return true;
+	    }
+	    var FreeSquares = $('td.square.free');
+	    $.each(FreeSquares, function(index, element) {
+		console.log(data);
+		console.log('checking square['+element.getAttribute('data-y')+', '+element.getAttribute('data-x')+'] when the square['+data.square.y+', '+data.square.x+'] needs to be updated');
+		if (element.getAttribute('data-y') == data.square.y && element.getAttribute('data-x') == data.square.x) {
+		    App.Clock.reset(true);
+		    element.className = element.className.replace('free', 'played');
+		    element.id = data.user_id;
+		    console.log('case updated by the challenger');
+		    $('h1#status-game').text('It\'s yout turn to play!');
+		    return false;
+		}
+	    });
+	},
+
+	cheat: function() {
+	    alert('Someone tried to cheat !!');
+	}
+    },
+
+    Player: {
+	socket: {},
+	id: '',
+	
+	initialize: function() {
+	    this.socket = io.connect('http://localhost:4250');
+	    this.socket.on('wait', this.wait);
+	    this.socket.on('start', this.start);
+	    this.socket.on('play', App.Playground.update);
+	    this.socket.on('cheat', App.Playground.cheat);
+	    this.socket.on('victory', App.Scenarios.victory);
+	    this.socket.on('equality', App.Scenarios.equality);
+	},
+
+	wait: function(data) {
+	    console.log('Player created with id['+data.user_id+']');
+	    App.Player.id = data.user_id;
+	},
+
+	start: function(data) {
+	    App.Clock.init(data.user_id, data.timer);
+	},
+
+	play: function(square) {
+	    this.socket.emit('play', square);
+	}
+    },
+    
+    start: function(options) {
+	this.Player.initialize();
+	//Waiting for a challenger...
+    }
+};
+
+$(document).ready(function(){
+    App.start({ timer: 5000 });
+});
---
+++ source/Server/server.js
@@ -0,0 +1,265 @@
+#!/usr/bin/env node
+var io = require('socket.io').listen(4250);
+
+function OGame(id) {
+    var that = this;
+
+    this.onGame = 'B';
+
+    this.room = id;
+
+    this.Players = [];
+
+    this.Playground = {
+	map: [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ],
+
+	getMap: function() {
+	    return this.map;
+	},
+
+	update: function(user_id, square) {
+	    console.log('Update square['+square.y+', '+square.x+'] by user['+user_id+'] actually marked['+this.map[square.y][square.x]+'] user onGame['+that.onGame+']');
+	    if (user_id != that.onGame) {
+		io.sockets.in(this.room).emit('cheat', { user_id: user_id, square: square, feedback: 'the user '+user_id+' tried to play more than 1 time in its turn!' });
+		return false;
+	    }
+		
+	    if (this.map[square.y][square.x]) { 
+		io.sockets.in(this.room).emit('cheat', { user_id: user_id, square: square, feedback: 'the user '+user_id+' trie to steale a square already marked by his challenger!' });
+		return false;
+	    }
+
+	    this.map[square.y][square.x] = user_id;
+	    that.onGame = (that.onGame == 'A' ? 'B' : 'A');
+	    io.sockets.in(this.room).emit('play', { user_id: user_id, square: square });
+	    return true;
+	}
+    };
+
+    this.Clock = {
+	delay: 10,
+	engine: 0,
+		
+	start: function() {
+	    this.delay = 10;
+	    var self = this;
+	    this.engine = setInterval(function() {
+		if (!self.delay) {
+		    //that.Players[that.onGame].username
+		    io.sockets.in(self.room).emit('victory', { user_id: that.onGame, feedback: 'The user '+(that.onGame == 'A' ? 'B' : 'A')+' has consumed all his time.' });
+		    self.stop();
+		}
+		self.delay--;
+	    }, 1000);	    
+	},
+
+	stop: function() {
+	    clearInterval(this.engine);
+	},
+	
+	reset: function(room, socket) {
+	    this.delay = 10;
+	},
+	
+    };
+    
+    this.getRoom = function() {
+	return this.room;
+    };
+
+    this.setRoom = function(room) {
+	this.room = room;
+    };
+
+    this.getNbPlayers = function() {
+	return this.Players.length;
+    };
+
+    this.addPlayer = function(Player) {
+	if (this.Players.lenght == 2)
+	    return false;
+
+	this.Players.push(Player);
+	return true;
+    };
+
+    this.checkVictory = function(user_id, square, callback) {
+	console.log('Checking victory concernant le user['+user_id+'] de la room['+this.room+'] après avoir joué la case['+square.y+', '+square.x+']');
+	var map = this.Playground.getMap();
+	console.log('map['+map+']');
+
+	if (map[0][square.x] == user_id && map[1][square.x] == user_id && map[2][square.x] == user_id) {
+	    console.log('ordo victory');
+	    io.sockets.in(this.room).emit('victory', { user_id: user_id, feedback: 'The user '+user_id+' win!' });
+	    callback(true);
+	    return true;
+	}
+
+	if (map[square.y][0] == user_id && map[square.y][1] == user_id && map[square.y][2] == user_id) {
+	    console.log('abs victory');
+	    io.sockets.in(this.room).emit('victory', { user_id: user_id, feedback: 'The user '+user_id+' win!' });
+	    callback(true);
+	    return true;
+	}
+
+	if (map[1][1] != user_id) {
+	    callback(false);
+	    return false;
+	}
+
+	if ((map[0][0] == user_id && map[2][2] == user_id) || map[2][0] == user_id && map[0][2] == user_id) {
+	    console.log('diag victory');
+	    io.sockets.in(this.room).emit('victory', { user_id: user_id, feedback: 'The user '+user_id+' win!' });
+	    callback(true);
+	    return true;
+	}
+	callback(false);
+    };
+
+    this.checkEquality = function(callback) {
+	var map = this.Playground.getMap();
+	var full = true;
+	for (var y = 0; y < 3 ; y++) {
+	    for (var x = 0; x < 3 ; x++) {
+		if (!map[y][x])
+		    full = false;
+	    }
+	}
+	if (full) {
+	    console.log('EQUALITY!');
+	    io.sockets.in(this.room).emit('equality');
+	}
+	callback(full);
+    };
+
+    this.start = function() {
+	var nb_players = this.getNbPlayers();
+	this.Players[nb_players - 1].emit('wait', { user_id: this.Players[nb_players - 1].user_id });
+	if (nb_players == 2) {
+	    this.onGame = this.Players[nb_players - 1].user_id;
+	    io.sockets.in(this.getRoom()).emit('start', { timer: 10, user_id: this.Players[nb_players - 1].user_id });
+	    this.Clock.start();
+	}
+    };
+}
+
+
+var Server = {
+    Games: [],
+
+    assignUser: function(Player) {
+	Player.user_id = 'A';
+	Player.username = 'Gerard';
+
+	console.log('nb_games['+this.Games.length+']');
+	for (Game in this.Games) {
+	    var nb_players = this.Games[Game].getNbPlayers();
+	    console.log('checking the room['+Game+'] nb_playres['+nb_players+']');
+	    if (nb_players < 2) {
+		console.log('User joined an existing room['+Game+']');
+		
+		if (nb_players == 1) { 
+		    Player.user_id = 'B';
+		    Player.username = 'Jean Claude';
+		}
+
+		Player.join(Game);
+		this.Games[Game].addPlayer(Player);
+		this.Games[Game].start();
+		return true;
+	    }
+	}
+
+	var game_id = Server.generateRoomName();
+	Player.join(game_id);
+	var Game = new OGame(game_id);
+	Game.addPlayer(Player);
+	this.Games[game_id] = Game;
+	this.Games[game_id].start();
+    },
+
+    generateRoomName: function() {
+	var key =  'a';
+	while (typeof this.Games[key] != 'undefined') {
+	    key += 'a';
+	}
+	return key;
+    },
+
+    addGame: function(Game) {
+	if (!Game instanceof Game) {
+	    return false;
+	}
+	Game.setRoom(this.generateRoomName());
+	this.Games[Game.getRoom()] = Game;
+	return true;
+    },
+
+    deleteGame: function(room) {
+	if (typeof this.Games[room] != 'undefined') {
+	    this.Games[room].Clock.stop();
+	    delete this.Games[room];
+	    console.log('room['+room+'] delete OK');
+	    return true;
+	}
+	console.log('room['+room+'] delete KO');
+	return false;
+    }
+};
+
+
+io.sockets.on('connection', function(Player) {
+
+    function playCallback(square) {
+	console.info('|-->User['+Player.id+'] Play on ['+square.y+', '+square.x+']');
+	var user_rooms = io.sockets.manager.roomClients[Player.id];
+	for (user_room in user_rooms) {
+	    console.log('user_room['+user_room+'] and value['+user_rooms[user_room]+'] from user['+Player.user_id+']');
+	    if (user_room == '')
+		continue;
+	    
+	    console.log('square maked['+Server.Games[user_room.substring(1)].Playground.getMap()[square.y][square.x]+'] before any server proccessing');
+
+	    if (typeof Server.Games[user_room.substring(1)] != 'undefined'/* && this.Games[user_room.substring(1)].secret == Player.secret */) {
+		if (!Server.Games[user_room.substring(1)].Playground.update(Player.user_id, square)) {
+		    console.log('CHEATER DETECTED');
+		}
+		Server.Games[user_room.substring(1)].checkVictory(Player.user_id, square, function(is_over) {
+	    	    if (is_over) {
+			Server.deleteGame(user_room.substring(1));
+			return true;
+		    }
+		    Server.Games[user_room.substring(1)].checkEquality(function(is_over) {
+			if (is_over) {
+			    Server.deleteGame(user_room.substring(1));
+			    return true;
+			}
+			Server.Games[user_room.substring(1)].Clock.reset(user_room, Player);
+		    });
+		});
+	    }
+	}
+	console.info('|-->User['+Player.id+'] Played on ['+square.y+', '+square.x+']');
+    }
+   
+    function disconnectCallback() {
+	console.info('|-->User['+Player.user_id+'] disconnect');
+	var user_rooms = io.sockets.manager.roomClients[Player.id];
+	for (user_room in user_rooms) {
+	    console.log('user_room['+user_room+'] and value['+user_rooms[user_room]+'] from user['+Player.user_id+']');
+	    if (user_room == '')
+		continue;
+	    
+	    if (typeof Server.Games[user_room.substring(1)] != 'undefined'/* && this.Games[user_room.substring(1)].secret == Player.secret */) {
+		io.sockets.in(user_room.substring(1)).emit('victory', { user_id: (Player.user_id == 'A' ? 'B' : 'A'), feedback: 'The user '+Player.user_id+' has abandonned the game.' });
+		Server.deleteGame(user_room.substring(1));
+	    }
+	}
+	console.info('|-->User['+Player.user_id+'] disconnected');
+    }
+
+    Player.on('play', playCallback);
+    Player.on('disconnect', disconnectCallback);
+
+    Server.assignUser(Player);
+});
